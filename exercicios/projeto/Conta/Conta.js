4ece0dec17e7b9046b1731c739a81ff4e4501953
talu <talita.santos96@hotmail.com>
2023-11-03 23:03:28 -0300

    [async] Add comentários e método sacar na classe Conta

--- exercicios/projeto/Conta/Conta.js
+++ exercicios/projeto/Conta/Conta.js
@@ -1,3 +1,4 @@
+/* parte quatro --> implementação da classe com seus respectivos métodos e atributos. eles devem ser coerentes ao que foi escrito nos testes.*/
 class Conta {
     #agencia;
     #conta;
@@ -26,6 +27,10 @@ class Conta {
         return this.#saldo;
     }
 
+    setSaldo(novoSaldo) {
+        this.#saldo = novoSaldo;
+    }
+
     criarConta(agencia, conta, saldo) {
         if (agencia.length === 4 && conta.length === 5 && saldo > 0) {
             //faltou eu fazer o construtor........ :clown:
@@ -35,6 +40,20 @@ class Conta {
             return "Conta criada com sucesso!"
         } else {
             throw new Error("Dados inválidos para cadastro.");
+        }
+    }
+
+    sacar(valor) {
+        if (valor > 0 && typeof valor === "number") {
+            if (this.#saldo - valor > 0) {
+                const saldo = this.#saldo - valor;
+                this.setSaldo(saldo);
+            } else {
+                throw new Error("Saldo insuficiente para saque.");
+            }
+
+        } else {
+            throw new Error("Valor inválido para saque!");
         }
     }
 }
