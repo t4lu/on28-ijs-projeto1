2f8fd8bcf2ac7cec08e3d86440f5d3fe3fdc781c
talu <talita.santos96@hotmail.com>
2023-11-05 14:03:13 -0300

    [async] Add testes para o método criarChavesPix

--- exercicios/projeto/Conta/Conta.test.js
+++ exercicios/projeto/Conta/Conta.test.js
@@ -65,7 +65,7 @@ describe("Testes da classe Conta", () => {
         expect(() => conta.sacar(-100)).toThrow("Saldo insuficiente");
         expect(conta.saldo).toBe(1000);
     });
-    
+
     test("Retorna mensagem de sucesso ao depositar valor em conta.", () => {
         const conta = new Conta();
         conta.criarConta("1234", "12345", 1000);
@@ -88,5 +88,61 @@ describe("Testes da classe Conta", () => {
 
         expect(() => conta.depositar("100")).toThrow("Valor inválido para depósito.");
         expect(conta.saldo).toBe(1000);
+    });
+
+    test("Deve criar uma chave pix por CPF -- do tipo string -- com sucesso.", () => {
+        //setup - define / monta / estrutura o objeto que precisa ser testado (mock)
+        const conta = new Conta();
+
+        //ação - o que será feito com / por / pelo objeto
+        const operacao = conta.criarChavePix("12345678910", "CPF");/*vai dar erro, o método ainda não existe. nos parametros,
+        o objetivo do método vai ser analisar se é um CPF, email ou telefone. essa informação deve ser retornada ao usuário.*/
+
+        //verificação - sintaxe para descrever o que se espera de resultado da ação; aqui deve-se usar os matchers do jest
+        expect(operacao).toBe("CPF cadastrado como chave pix com sucesso!");
+
+        //aqui, consigo invocar os atributos que estão dentro do objeto chavesPix (ps: poderia ser um array. nao foi usada dessa forma pq, no mercado, quando se conecta ao banco de dados, é necessário passar "keys" e "values" --> cpf: undefined, por exemplo)
+        expect(conta.chavesPix.cpf).toBe("12345678910");
+    });
+
+    test("Deve retornar mensagem de erro, ao cadastrar chave pix com CPF inválido.", () => {
+        //setup
+        const conta = new Conta();
+
+        //ação
+        const operacao = conta.criarChavePix("451235", "CPF");
+
+        //verificação
+        expect(() => operacao).toThrow("Erro: CPF inválido!");
+    });
+
+    test("Deve criar uma chave pix por email -- do tipo string -- com sucesso.", () => {
+        const conta = new Conta();
+        const operacao = conta.criarChavePix("email@email.com", "email");
+
+        expect(operacao).toBe("Email cadastrado como chave pix com sucesso!");
+        expect(conta.chavesPix.email).toBe("email@email.com");
+    });
+
+    test("Deve retornar mensagem de erro ao cadastrar chave pix com e-mail inválido.", () =>{
+        const conta = new Conta();
+        const operacao = conta.criarChavePix("emailarrobaemailpontocom", "email");
+
+        expect(() => operacao).toThrow("Erro: E-mail inválido!");
+    });
+
+    test("Deve criar chave pix por telefone -- do tipo string -- com sucesso.", () =>{
+        const conta = new Conta();
+        const operacao = conta.criarChavePix("11 1234-5678", "telefone");
+
+        expect(operacao).toBe("Telefone cadastrado como chave pix com sucesso!");
+        expect(conta.chavesPix.telefone).toBe("11 1234-5678");
+    });
+
+    test("Deve retornar mensagem de erro, ao cadastrar chave pix com telefone inválido.", () => {
+        const conta = new Conta();
+        const operacao = conta.criarChavePix("11111111", "telefone");
+
+        expect(() => operacao).toThrow("Erro: telefone inválido!");
     });
 });
