efd8957aed110ba6a2f5a6c3c12a524c1c8675e8
talu <talita.santos96@hotmail.com>
2023-11-05 14:51:48 -0300

    [async] Add método criarChavePix na classe Conta

--- exercicios/projeto/Conta/Conta.js
+++ exercicios/projeto/Conta/Conta.js
@@ -57,12 +57,43 @@ class Conta {
         }
     }
 
-    depositar(valor){
-        if(valor > 0 && typeof valor === "number") {
+    depositar(valor) {
+        if (valor > 0 && typeof valor === "number") {
             const saldoAtualizado = this.#saldo += valor;
             this.setSaldo(saldoAtualizado);
         } else {
             throw new Error("Valor inválido para depósito.");
+        }
+    }
+
+    criarChavePix(chavePix, tipo) {
+        switch (tipo) {
+            case "CPF":
+                let regexCPF = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
+                if (regexCPF.test(chavePix)) {
+                    this.chavesPix.cpf = chavePix;
+                    return "CPF cadastrado como chave pix com sucesso!";
+                } else {
+                    throw new Error("Erro: CPF inválido!")
+                }
+            case "EMAIL":
+                let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;;
+                if (regexEmail.test(chavePix)) {
+                    this.chavesPix.email = chavePix;
+                    return "Email cadastrado como chave pix com sucesso!";
+                } else {
+                    throw new Error("Erro: E-mail inválido!")
+                }
+            case "TELEFONE":
+                let regexTelefone = /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/;
+                if (regexTelefone.test(chavePix)) {
+                    this.chavesPix.telefone = chavePix;
+                    return "Telefone cadastrado como chave pix com sucesso!";
+                } else {
+                    throw new Error("Erro: Telefone inválido!")
+                }
+            default:
+                return "Chave pix não existe.";
         }
     }
 }
